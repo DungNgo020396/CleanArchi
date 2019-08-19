bb0560a00d1711a1e6204262da2259924bae24d4
Ngo Thi Thuy Dung (VTI.D1) <dung.ngothithuy@vti.vn>
2019-08-19 17:57:54 +0700

    HHH

--- app/src/main/java/com/example/cleanarchtest/net/RestApi.java
+++ app/src/main/java/com/example/cleanarchtest/net/RestApi.java
@@ -7,6 +7,7 @@ import io.reactivex.Observable;
 public abstract class RestApi {
     String API_BASE_URL = "https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture/";
     String API_URL_GET_USER_LIST = API_BASE_URL + "users.json";
+    String API_URL_GET_USER_DETAILS = API_BASE_URL + "user_";
      abstract Observable<List<UserEntity>> userEntityList();
      abstract Observable<UserEntity> userEntityById(final int userId);
 }
--- app/src/main/java/com/example/cleanarchtest/net/RestApiImpl.java
+++ app/src/main/java/com/example/cleanarchtest/net/RestApiImpl.java
@@ -7,54 +7,96 @@ import android.net.NetworkInfo;
 
 import com.example.cleanarchtest.entities.UserEntity;
 import com.example.cleanarchtest.entities.mapper.UserEntityJsonMapper;
+import com.example.cleanarchtest.exception.NetworkConnectionException;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.List;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 
 public class RestApiImpl extends RestApi {
-    private Context context;
-    private UserEntityJsonMapper userEntityJsonMapper;
-    public RestApiImpl(Context context, UserEntityJsonMapper userEntityJsonMapper) {
-        this.context=context;
-        this.userEntityJsonMapper=userEntityJsonMapper;
+    private final Context context;
+    private final UserEntityJsonMapper userEntityJsonMapper;
 
+    /**
+     * Constructor of the class
+     *
+     * @param context              {@link android.content.Context}.
+     * @param userEntityJsonMapper {@link UserEntityJsonMapper}.
+     */
+    public RestApiImpl(Context context, UserEntityJsonMapper userEntityJsonMapper) {
+        if (context == null || userEntityJsonMapper == null) {
+            throw new IllegalArgumentException("The constructor parameters cannot be null!!!");
+        }
+        this.context = context.getApplicationContext();
+        this.userEntityJsonMapper = userEntityJsonMapper;
     }
-    // check network:
+
+    @Override
+    public Observable<List<UserEntity>> userEntityList() {
+        return Observable.create(emitter -> {
+            if (isThereInternetConnection()) {
+                try {
+                    String responseUserEntities = getUserEntitiesFromApi();
+                    if (responseUserEntities != null) {
+                        emitter.onNext(userEntityJsonMapper.transformUserEntityCollection(
+                                responseUserEntities));
+                        emitter.onComplete();
+                    } else {
+                        emitter.onError(new NetworkConnectionException());
+                    }
+                } catch (Exception e) {
+                    emitter.onError(new NetworkConnectionException(e.getCause()));
+                }
+            } else {
+                emitter.onError(new NetworkConnectionException());
+            }
+        });
+    }
+
+    @Override
+    public Observable<UserEntity> userEntityById(final int userId) {
+        return Observable.create(emitter -> {
+            if (isThereInternetConnection()) {
+                try {
+                    String responseUserDetails = getUserDetailsFromApi(userId);
+                    if (responseUserDetails != null) {
+                        emitter.onNext(userEntityJsonMapper.transformUserEntity(responseUserDetails));
+                        emitter.onComplete();
+                    } else {
+                        emitter.onError(new NetworkConnectionException());
+                    }
+                } catch (Exception e) {
+                    emitter.onError(new NetworkConnectionException(e.getCause()));
+                }
+            } else {
+                emitter.onError(new NetworkConnectionException());
+            }
+        });
+    }
+
+    private String getUserEntitiesFromApi() throws IOException {
+        return ApiConnection.createGET(API_URL_GET_USER_LIST).requestSyncCall();
+    }
+
+    private String getUserDetailsFromApi(int userId) throws IOException {
+        String apiUrl = API_URL_GET_USER_DETAILS + userId + ".json";
+        return ApiConnection.createGET(apiUrl).requestSyncCall();
+    }
+
+    /**
+     * Checks if the device has any active internet connection.
+     *
+     * @return true device with internet connection, otherwise false.
+     */
     private boolean isThereInternetConnection() {
         boolean isConnected;
-
         ConnectivityManager connectivityManager =
                 (ConnectivityManager) this.context.getSystemService(Context.CONNECTIVITY_SERVICE);
         @SuppressLint("MissingPermission") NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         isConnected = (networkInfo != null && networkInfo.isConnectedOrConnecting());
 
         return isConnected;
-    }
-
-    @Override
-    Observable<List<UserEntity>> userEntityList() {
-
-        return Observable.create(new ObservableOnSubscribe<List<UserEntity>>() {
-            @Override
-            public void subscribe(ObservableEmitter<List<UserEntity>> emitter) throws Exception {
-                if(isThereInternetConnection()){
-                    String responseUserDetails = getUserDetailsFromApi(userId);
-
-                }
-            }
-        });
-
-    }
-
-    @Override
-    Observable<UserEntity> userEntityById(int userId) {
-        return null;
-    }
-    private String getUserEntitiesFromApi() throws MalformedURLException {
-        return ApiConnection.createGET(API_URL_GET_USER_LIST).requestSyncCall();
     }
 }
